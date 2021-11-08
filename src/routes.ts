f40dfd4cbab0f09ff8f9120d222d0b63afcdf429
sofiaarend21 <sofiaarend@gaudium.com.br>
2021-11-07 21:39:37 -0300

    ajuste de rotas, disciplina e turma por id e rotas de patch

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { createDisciplina } from './services/disciplina/createDisciplina';
 import { updateDisciplina } from "./services/disciplina/updateDisciplina";
 import { getDisciplinas } from "./services/disciplina/getDisciplinas";
-import { createTurma } from "./services/turma/createTurma";
-import { getTurmas } from "./services/turma/getTurmas";
-import { updateTurma } from "./services/turma/updateTurma";
-import { deleteDisciplina } from "./services/disciplina/deleteDisciplina";
+import { getDisciplinaById } from "./services/disciplina/getDisciplinaById";
 import { getDisciplinasByValidade } from "./services/disciplina/getDisciplinasByValidade";
-import { deleteTurma } from "./services/turma/deleteTurma";
+import { deleteDisciplina } from "./services/disciplina/deleteDisciplina";
+import { createTurma } from "./services/turma/createTurma";
+import { updateTurma } from "./services/turma/updateTurma";
+import { getTurmas } from "./services/turma/getTurmas";
+import { getTurmaById } from './services/turma/getTurmaById';
 import { getTurmasByHorario } from './services/turma/getTurmasByHorario';
+import { deleteTurma } from "./services/turma/deleteTurma";
 
 
 const router = Router();
@@ -20,7 +22,11 @@ router.get('/disciplina', (request, response) => {
 });
 
 router.get('/disciplina/:id', (request, response) => {
-    return getDisciplinasByValidade(request, response, request.params.id);
+    return getDisciplinaById(request, response, request.params.id);
+});
+
+router.get('/disciplina/validade', (request, response) => {
+    return getDisciplinasByValidade(request, response);
 });
 
 router.post('/disciplina', (request, response) => {
@@ -28,6 +34,10 @@ router.post('/disciplina', (request, response) => {
 });
 
 router.put('/disciplina/:id', (request,response) => {
+    return updateDisciplina(request,response);
+});
+
+router.patch('/disciplina/:id', (request,response) => {
     return updateDisciplina(request,response);
 });
 
@@ -42,7 +52,11 @@ router.get('/turma', (request, response) => {
 });
 
 router.get('/turma/:id', (request, response) => {
-    return getTurmasByHorario(request, response, request.params.id);
+    return getTurmaById(request, response, request.params.id);
+});
+
+router.get('/turma/horario', (request, response) => {
+    return getTurmasByHorario(request, response);
 });
 
 router.post('/turma', (request, response) => {
@@ -51,7 +65,10 @@ router.post('/turma', (request, response) => {
 
 router.put('/turma/:id', (request,response) => {
      return updateTurma(request,response);
+});
 
+router.patch('/turma/:id', (request,response) => {
+    return updateTurma(request,response);
 });
 
 router.delete('/turma/:id', (request, response) => {
